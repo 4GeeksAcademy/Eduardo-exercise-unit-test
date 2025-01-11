6ec71e80e3de86e3c740fd0c9e72b5f97d454e41
EduardoGP <124112815+Eduardo7052311@users.noreply.github.com>
2025-01-11 02:02:17 +0000

    actualizacion

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-let oneEuroIs = {
-    "JPY": 156.5, // japan yen
-    "USD": 1.07, // us dollar
-    "GBP": 0.87, // british pound
-}
-function fromEuroToDollar(euro){
-      let dollar = euro * oneEuroIs.USD
-      return dollar
-}
-module.exports = {fromEuroToDollar, oneEuroIs}
+let oneEuroIs = { 
+    "JPY": 156.5, // japan yen 
+     "USD": 1.07, // us dollar 
+     "GBP": 0.87, // british pound 
+     }; 
+     function fromEuroToDollar(euro) { 
+        let dollar = euro * oneEuroIs.USD; 
+        return dollar; 
+    } 
+    function fromEuroToYen(euro) { 
+        let yen = euro * oneEuroIs.JPY; 
+        return yen; } 
+    
+        function fromEuroToPound(euro) { 
+        let pound = euro * oneEuroIs.GBP; return pound; 
+    }   
+    module.exports = { fromEuroToDollar, fromEuroToYen, fromEuroToPound, oneEuroIs };
